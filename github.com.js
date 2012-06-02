794049adccc8b8fc3095939ce6e6e337d371e62a
Eric J. Duran <eric.duran7@gmail.com>
2012-06-02 09:54:12 -0400

    Adding delete all option to github

---
+++ github.com.js
@@ -0,0 +1,27 @@
+/**
+ * Github.com
+ * Add a Delete All button next to read all, in the inbox.
+ */
+
+(function() {
+  // Add a "Delete All" option when we're on our inbox.
+  if ($('#inbox').length) {
+    $del = $('<a class="minibutton">').click(function () {
+      $('.mini-icon-delete-note').click();
+      // Give it some time then reload to delete everything.
+      window.setTimeout(function() {
+        window.location.reload();
+      }, 500);
+    }).html('Delete All');
+
+    $p = $('#inbox h1').siblings('p');
+    if ($p.length) {
+      // Lets add a delete all options.
+      $p.prepend($del);
+    }
+    else {
+      $p = $('<p style="margin-top:5px;margin-bottom:-10px;text-align:right">');
+      $p.html($del).append('<div class="rule">').insertAfter($('#inbox h1'));
+    }
+  }
+})();
